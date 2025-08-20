dc6acf1321b5c9c43de20028e9201585ef67f051
Pablo Gomez <pablogg011@gmail.com>
2025-08-20 18:39:40 -0300

    Se crea el proyecto base del frontend de la app con vite+react+typescript+tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-8 bg-blue-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-800">Hola Tailwind 4 🚀</h1>
+    </div>
+  )
+}
+
+export default App
